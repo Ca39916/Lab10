0ff3f9495f44c585f710221db8fab27d5e86aa5a
Camila Rezende <csalinor@emeal.nttdata.com>
2022-01-06 11:01:58 -0300

    ExercicioLab10

---
+++ Laboratorio10/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace Laboratorio10
+{
+    internal class Program
+    {
+        private static void TrataLimiteSuperior(string msg)
+        {
+            Console.WriteLine(msg);
+        }
+
+        static void Main(string[] args)
+        {
+            // inicializa o termometro e mostra seu valor
+            TermometroLimite term = new TermometroLimite(5);
+            Console.WriteLine(term.ToString());
+            Console.ReadKey();
+
+            // adiciona um tratador ao evento LimiteSuperiorEvent
+            term.LimiteSuperiorEvent += new TermometroLimite.MeuDelegate(TrataLimiteSuperior);
+            term.NormalizadoEvent += new TermometroLimite.MeuDelegate(TrataLimiteSuperior);
+
+            // aumentar a temperatura além do limite term.Aumentar(6);
+            Console.WriteLine(term.ToString());
+            Console.ReadKey();
+
+            Console.WriteLine();
+            Console.WriteLine("Aumentando a Temperatura em 6 graus");
+            term.Aumentar(6);
+            Console.ReadKey();
+
+
+            Console.WriteLine();
+            Console.WriteLine("Diminuindo a Temperatura em 4 graus");
+            term.Diminuir(4);
+            Console.ReadKey();
+               
+
+        }
+    }
+}
---
+++ Laboratorio10/TermometroLimite.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Laboratorio10
+{
+    internal class TermometroLimite : Termometro
+    {
+        private double limiteSuperior;
+        private bool disparadoEventoLimiteSuperior;
+        public TermometroLimite(double ls)
+        {
+            limiteSuperior = ls;
+            disparadoEventoLimiteSuperior = false;
+        }
+        public double LimiteSuperior
+        {
+            get { return limiteSuperior; }
+            set { limiteSuperior = value; }
+        }
+        public delegate void MeuDelegate(string msg);
+
+        public event MeuDelegate LimiteSuperiorEvent;
+
+        private void OnLimiteSuperiorEvent()
+        {
+            // verifica se a temperatura ultrapassou o limite e
+            // Verifica se o evento já foi disparado, para não disparar continuamente.
+            // Verifica se a temperatura esta maior que o limite e se anteriormente a temperatura estava normal (disparadoEventoLimiteSuperior = false)
+            if ((this.Temperatura > limiteSuperior) && (!disparadoEventoLimiteSuperior))//disparadoEventoLimiteSuperior for falso ele mostra a msg 
+                // verifica se há tratador
+                if (LimiteSuperiorEvent != null)
+                {
+                    LimiteSuperiorEvent("Atenção: temperatura acima do limite!!!");
+                    disparadoEventoLimiteSuperior = true;
+                }
+        }
+
+        public override void Aumentar()
+        {
+            base.Aumentar();
+            OnLimiteSuperiorEvent();
+        }
+        public override void Aumentar(double quantia)
+        {
+            base.Aumentar(quantia);
+            OnLimiteSuperiorEvent();
+        }
+        public override void Diminuir()
+        {
+            base.Diminuir();
+            OnNormalizadoEvent();
+
+
+        }
+        public override void Diminuir(double quantia)
+        {
+            base.Diminuir(quantia);
+            OnNormalizadoEvent();
+            
+        }
+
+        //Exercício 3.1
+        public event MeuDelegate NormalizadoEvent;
+        private void OnNormalizadoEvent()
+        {
+            if ((this.Temperatura <= limiteSuperior) && (disparadoEventoLimiteSuperior))
+                // verifica se há tratador
+                if (NormalizadoEvent != null)
+                {
+                    disparadoEventoLimiteSuperior = false;
+                    NormalizadoEvent("temperatura voltou ao normal!!!" + "Temperatura atual: " + Temperatura);
+                    
+                }
+
+        }
+
+
+    }
+        
+    
+}
